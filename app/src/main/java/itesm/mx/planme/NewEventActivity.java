6b5ab31a8a045c67e373045fefbfb3ada90e5523
napster2091 <a01674901@itesm.mx>
2016-10-22 16:24:06 -0500

    seba

---
+++ app/src/main/java/itesm/mx/planme/NewEventActivity.java
@@ -0,0 +1,13 @@
+package itesm.mx.planme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewEventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_event);
+    }
+}
