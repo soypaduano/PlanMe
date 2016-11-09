557f099a9ed48f61682f84434a0bd43e02687623
napster2091 <a01674901@itesm.mx>
2016-11-08 20:31:52 -0600

    seba

--- app/src/main/java/itesm/mx/planme/SignupActivity.java
+++ app/src/main/java/itesm/mx/planme/SignupActivity.java
@@ -10,7 +10,11 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -36,7 +40,7 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
 
-    private static final String TAG = "TAG_FIREBASE";
+    public static final String TAG = "SignUp";
 
 
     @Override
@@ -50,7 +54,7 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
         et_correo = (EditText) findViewById(R.id.et_correo);
         et_telefono = (EditText) findViewById(R.id.et_telefono);
         et_passwd = (EditText) findViewById(R.id.et_password);
-        et_passwd2 = (EditText) findViewById(R.id.et_passwd2);
+        et_passwd2 = (EditText) findViewById(R.id.et_password2);
 
         sp_año = (Spinner) findViewById(R.id.sp_año);
         sp_dia = (Spinner) findViewById(R.id.sp_dia);
@@ -64,6 +68,7 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
         mAuth = FirebaseAuth.getInstance();
 
         mAuthListener = new FirebaseAuth.AuthStateListener() {
+
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 FirebaseUser user = firebaseAuth.getCurrentUser();
@@ -122,7 +127,49 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
     @Override
     public void onClick(View view) {
 
-        Intent myIntent = new Intent(this, BuscarOfrecerActivity.class);
-        startActivity(myIntent);
+        String password1;
+        String password2;
+        String correo;
+
+        password1 = String.valueOf(et_passwd.getText());
+        password2 = String.valueOf(et_passwd2.getText());
+        correo = String.valueOf(et_correo.getText());
+
+        if(isValidEmailAddress(correo)==true && (password1.equals(password2))==true){
+            mAuth.createUserWithEmailAndPassword(correo, password1)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            Log.d(TAG, "createUserWithEmail:onComplete:" + task.isSuccessful());
+
+                            // If sign in fails, display a message to the user. If sign in succeeds
+                            // the auth state listener will be notified and logic to handle the
+                            // signed in user can be handled in the listener.
+                            if (!task.isSuccessful()) {
+                                Toast.makeText(getApplicationContext(), "Signup failed, try again",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+
+                            // ...
+                        }
+                    });
+            }
+        if(mAuth.getCurrentUser()!=null){
+            Intent myIntent = new Intent(this, BuscarOfrecerActivity.class);
+            startActivity(myIntent);
+
+        }
+        else
+            Toast.makeText(getApplicationContext(), "Correo or Contrasena incorecta",
+                    Toast.LENGTH_SHORT).show();
+
+
+    }
+
+    public boolean isValidEmailAddress(String correo) {
+        String ePattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
+        java.util.regex.Pattern p = java.util.regex.Pattern.compile(ePattern);
+        java.util.regex.Matcher m = p.matcher(correo);
+        return m.matches();
     }
 }
