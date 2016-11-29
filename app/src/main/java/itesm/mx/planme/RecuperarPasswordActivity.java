1ba2db48ab6760d52c866746bd30b0059a629808
napster2091 <a01674901@itesm.mx>
2016-11-29 12:38:00 -0600

    ui corrections

--- app/src/main/java/itesm/mx/planme/RecuperarPasswordActivity.java
+++ app/src/main/java/itesm/mx/planme/RecuperarPasswordActivity.java
@@ -47,14 +47,26 @@ public class RecuperarPasswordActivity extends AppCompatActivity implements View
 
         String email = et_email.getText().toString();
 
-        mAuth.sendPasswordResetEmail(email);
-        //TO-DO enviar correo para recuperarPassword
+        if(!email.equals("") && isValidEmailAddress(email)==true){
+            mAuth.sendPasswordResetEmail(email);
+            //TO-DO enviar correo para recuperarPassword
 
-        Toast.makeText(getApplicationContext(), R.string.toastMsgGetPass,Toast.LENGTH_LONG).show();
-        finish();
+            toastmsg(getString(R.string.toastMsgGetPass));
+            finish();
+        }
+
+        else
+            toastmsg(getString(R.string.wrongemail));
     }
 
-    public void prova(){
+    public boolean isValidEmailAddress(String email) {
+        String ePattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
+        java.util.regex.Pattern p = java.util.regex.Pattern.compile(ePattern);
+        java.util.regex.Matcher m = p.matcher(email);
+        return m.matches();
+    }
 
+    public void toastmsg(String msg) {
+        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
     }
 }
