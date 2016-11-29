1c86b462517665f7d316bbdfcafe319545d59f51
soypaduano <sebastian.paduano.m@gmail.com>
2016-11-28 22:50:56 -0600

    He cambiado los fonts de algunos botones y algunos de textviews.. Tambien he anadido el icono del IC_LAUNCHER.

--- app/src/main/java/itesm/mx/planme/EditarPerfilActivity.java
+++ app/src/main/java/itesm/mx/planme/EditarPerfilActivity.java
@@ -1,12 +1,16 @@
 package itesm.mx.planme;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.vision.text.Text;
 
 public class EditarPerfilActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -18,6 +22,7 @@ public class EditarPerfilActivity extends AppCompatActivity implements View.OnCl
     Button btn_actualizarFoto;
     Button btn_actualizarPassword;
     Button btn_guardarCambios;
+    TextView tv_editarPerfil;
 
 
     @Override
@@ -31,14 +36,35 @@ public class EditarPerfilActivity extends AppCompatActivity implements View.OnCl
         et_editarTelefono = (EditText) findViewById(R.id.et_editarTelefono);
         et_editarDescripcion = (EditText) findViewById(R.id.et_editarDescripcion);
 
+        tv_editarPerfil = (TextView) findViewById(R.id.tvEditProfileTitle);
+
         btn_actualizarFoto = (Button) findViewById(R.id.btn_actualizarFoto);
         btn_actualizarPassword = (Button) findViewById(R.id.btn_actualizarPassword);
         btn_guardarCambios = (Button) findViewById(R.id.btn_guardarCambios);
+
+
+
 
         btn_actualizarFoto.setOnClickListener(this);
         btn_actualizarPassword.setOnClickListener(this);
         btn_guardarCambios.setOnClickListener(this);
 
+
+
+
+        Typeface type = Typeface.createFromAsset(getAssets(),"fonts/Railway.otf");
+
+        et_editarNombre.setTypeface(type);
+        et_editarApellido.setTypeface(type);
+        et_editarUsername.setTypeface(type);
+        et_editarTelefono.setTypeface(type);
+        et_editarDescripcion.setTypeface(type);
+
+        btn_actualizarFoto.setTypeface(type);
+        btn_actualizarPassword.setTypeface(type);
+        btn_guardarCambios.setTypeface(type);
+
+        tv_editarPerfil.setTypeface(type);
 
 
     }
